7ef3b1f489327de4efcaff6e4bd4789d19b57926
jason <hrndzjason@gmail.com>
2022-10-05 21:29:35 -0500

    currently error free

--- script.js
+++ script.js
@@ -14,11 +14,12 @@ let turn = true
 let darkPoints = 12
 let lightPoints = 12
 let selectedTiles
+
 let currentChecker = {
   checker: -1,
   idxOfChecker: -1,
   isKing: false,
-  //board needs all possible starting winning combos declared in variable, set to false.
+  //board need`s` all possible starting winning combos declared in variable, set to false.
   // need at least 8, four for each side
   tileSeven: false,
   tileNine: false,
@@ -28,6 +29,10 @@ let currentChecker = {
   tileMinusNine: false,
   tileMinusFourteen: false,
   tileMinusEighteen: false
+}
+let findChecker = function(checker) {
+  let parse = parseInt(checker)
+  return board.indexOf(parse)
 }
 
 const tiles = document.querySelectorAll('div')
@@ -47,7 +52,7 @@ function handleTileClick() {
       lightChecker[i].addEventListener('click', handleLightDarkCheckers)
   }
   
-  console.log(handleTileClick)
+  //console.log(handleTileClick)
 }
 
 function handleLightDarkCheckers() {
@@ -59,17 +64,72 @@ function handleLightDarkCheckers() {
   removeHandleChecker()
   resetPiece()
 }
+
 function removeHandleChecker() {
   for (let i = 0; i < tiles.length; i++) {
     tiles[i].removeAttribute('onclick')
   }
   
 }
-function resetPiece() {
-  for(let i = 0; i < selectedTiles.length; i++) {
-    selectedTiles[i].style.border = '2px solid black'
-  }
-  console.log(resetPiece)
+
+
+function resetCurrentChecker(){
+  currentChecker.checker = -1
+  currentChecker.idxOfChecker = -1
+  currentChecker.isKing = false
+  currentChecker.tileSeven = false,
+  currentChecker.tileNine = false,
+  currentChecker.tileFourteen = false,
+  currentChecker.tileEighteen = false,
+  currentChecker.tileMinuseSeven = false,
+  currentChecker.tileMinusNine = false,
+  currentChecker.tileMinusFourteen = false,
+  currentChecker.tileMinusEighteen = false
 }
 
+function getCurrentChecker() {
+  currentChecker.checker = parseInt(event.target.id)
+  currentChecker.idxOfChecker = findChecker(currentChecker.checker)
+  kingCheckers()
+  console.log(getCurrentChecker)
+}
+function resetPiece() {
+  for(let i = 0; i < selectedTiles.length; i++) {
+    selectedTiles[i].style.border = '2px solid yellow'
+  }
+  //console.log(resetPiece)
+  resetCurrentChecker()
+  getCurrentChecker()
+}
+
+
+
+function kingCheckers(){
+  if(document.getElementById(currentChecker.checker).classList.contains('king')) {
+    currentChecker.isKing = true
+  } else {
+    currentChecker.isKing = false
+  }
+  availSpaces()
+}
+
+function availSpaces() {
+  if (board[currentChecker.idxOfChecker + 7] === null &&
+  tiles[currentChecker.idxOfChecker + 7].classList.contains('blank') !== true) {
+    currentChecker.tileSeven = true
+  }
+  if (board[currentChecker.idxOfChecker + 9] === null &&
+    tiles[currentChecker.idxOfChecker + 9].classList.contains('blank') !== true) {
+      currentChecker.tileNine = true
+  }
+  if (board[currentChecker.idxOfChecker - 7] === null &&
+    tiles[currentChecker.idxOfChecker - 7].classList.contains('blank') !== true) {
+      currentChecker.tileMinuseSeven = true
+    }
+    if (board[currentChecker.idxOfChecker - 9] === null &&
+      tiles[currentChecker.idxOfChecker - 9].classList.contains('blank') !== true) {
+        currentChecker.tileMinusNine = true
+      }
+}
+console.log(tiles)
 handleTileClick()
